794401f049c885618d5227d5d51d4deeaf7e1b36
he/him <132726465+baoduyne@users.noreply.github.com>
2024-02-22 17:50:10 +0700

    Update page7.java

--- page7.java
+++ page7.java
@@ -1,12 +1,13 @@
-public class Assignment2 {   
+public class Page7 {   
   public static void main(String[] args)
 { 
-  Number n1 = new Number();
- Number n2 = new Number();
+ Page7 n1 = new Page();
+ Page7 n2 = new Page();
  n1.i = 2;        n2.i = 5;        n1 = n2;    
-  n2.i = 10; // what is n1.i?      
-  n1.i = 20; // what is n2.i?   
+  n2.i = 10;     
+  n1.i = 20;  
+  System.out.println(n1.i);
+  System.out.println(n2.i);
 } 
 }
-//n1.i = 5
-//n2.i = 10
+
