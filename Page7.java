be6cab83dbbf82b61307ff801d69289ece96fc30
he/him <132726465+baoduyne@users.noreply.github.com>
2024-02-22 18:20:19 +0700

    Update and rename page7.java to Page7.java

--- Page7.java
+++ Page7.java
@@ -1,8 +1,8 @@
 public class Page7 {   
   public static void main(String[] args)
 { 
- Page7 n1 = new Page();
- Page7 n2 = new Page();
+ Page7 n1 = new Page7();
+ Page7 n2 = new Page7();
  n1.i = 2;        n2.i = 5;        n1 = n2;    
   n2.i = 10;     
   n1.i = 20;  
