a2815bd6341292d6b9ac7e38d5cad1f763f7278d
he/him <132726465+baoduyne@users.noreply.github.com>
2024-02-22 18:19:36 +0700

    Update and rename page6.java to Page6.java

--- Page6.java
+++ Page6.java
@@ -1,8 +1,8 @@
-public class Page { 
+public class Page6 { 
 public int i;
 public static void main(String[] args) {
-Page n1 = new Page();
-Page n2 = new Page();
+Page6 n1 = new Page6();
+Page6 n2 = new Page6();
 ainn1.i = 2;        n2.i = 5;        n1.i = n2.i;        n2.i = 10;
 System.out.println(n1.i);
 System.out.println(n2.i);
