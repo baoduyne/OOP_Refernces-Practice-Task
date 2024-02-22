54dbbcd09b612e7ea74b6ec4ca3f4b5218c00a49
he/him <132726465+baoduyne@users.noreply.github.com>
2024-02-22 17:29:30 +0700

    Create breakcontinue.java

---
+++ breakcontinue.java
@@ -0,0 +1,12 @@
+public void breakcontinue{
+  public static void main(String[] args){
+    int a = 10;
+    for(int i = 0 ; i <10 ; i++)
+      a--;
+      if(a==5){
+        break;}
+    if(a>5){
+      continue;
+    }
+  }
+}
