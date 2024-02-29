c89ce09ee7c60e87a981218cec702a45479e737b
he/him <132726465+baoduyne@users.noreply.github.com>
2024-02-29 16:20:57 +0700

    Update Page6.java

--- Page6.java
+++ Page6.java
@@ -3,7 +3,7 @@ public int i;
 public static void main(String[] args) {
 Page6 n1 = new Page6();
 Page6 n2 = new Page6();
-ainn1.i = 2;        n2.i = 5;        n1.i = n2.i;        n2.i = 10;
+n1.i = 2;        n2.i = 5;        n1.i = n2.i;        n2.i = 10;
 System.out.println(n1.i);
 System.out.println(n2.i);
 }}
