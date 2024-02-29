3f8789db4117dfa8ec35164fd1ceb4db02787727
he/him <132726465+baoduyne@users.noreply.github.com>
2024-02-29 16:25:08 +0700

    Update breakcontinue.java

--- breakcontinue.java
+++ breakcontinue.java
@@ -1,12 +1,16 @@
+import java.util.*;
+
 public class breakcontinue{
   public static void main(String[] args){
     int a = 10;
-    for(int i = 0 ; i <10 ; i++)
+    for(int i = 0 ; i <10 ; i++){
       a--;
       if(a==5){
         break;}
     if(a>5){
+      System.out.println(i);
       continue;
     }
   }
+}
 }
